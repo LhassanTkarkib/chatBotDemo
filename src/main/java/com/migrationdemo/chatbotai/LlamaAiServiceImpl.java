7524f7c5dda6d456f5582ec46ad76a05ed5c483e
Tkarkib Lhassan <73590332+LhassanTkarkib@users.noreply.github.com>
2024-05-30 12:46:33 +0100

    the chat is more optimized and the response is clean

--- src/main/java/com/migrationdemo/chatbotai/LlamaAiServiceImpl.java
+++ src/main/java/com/migrationdemo/chatbotai/LlamaAiServiceImpl.java
@@ -2,14 +2,13 @@ package com.migrationdemo.chatbotai;
 
 
 import org.springframework.ai.ollama.OllamaChatClient;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class LlamaAiServiceImpl implements LlamaAiService {
-
-
     private final OllamaChatClient chatClient;
+
+    public String Param = "Translate the following text to a SQL select query with no explanation and no decoration like / or /n just the sql request:";
 
     public LlamaAiServiceImpl(OllamaChatClient chatClient) {
         this.chatClient = chatClient;
@@ -17,8 +16,7 @@ public class LlamaAiServiceImpl implements LlamaAiService {
 
     @Override
     public LlamaResponse generateMessage(String promptMessage) {
-        final String llamaMessage = chatClient.call(promptMessage);
+        final String llamaMessage = chatClient.call(Param+promptMessage);
         return new LlamaResponse().setMessage(llamaMessage);
     }
-
 }
